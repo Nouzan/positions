ef2b1c148d4a969aa00a85b3bd05195a05820cb7
Nouzan <clobraindie@outlook.com>
2022-12-02 15:18:29 +0000

    add `Symbol` && parsing `Asset`

--- src/asset.rs
+++ src/asset.rs
@@ -10,7 +10,7 @@ use crate::{PositionNum, Positions};
 /// Asset.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[cfg_attr(feature = "serde", serde(from = "Str", into = "Str"))]
+#[cfg_attr(feature = "serde", serde(try_from = "Str", into = "Str"))]
 pub struct Asset {
     inner: Str,
 }
@@ -21,64 +21,63 @@ impl fmt::Display for Asset {
     }
 }
 
-// impl From<Asset> for Str {
-//     #[inline]
-//     fn from(asset: Asset) -> Self {
-//         asset.inner
-//     }
-// }
+/// Parse asset error.
+#[derive(Debug)]
+#[cfg_attr(feature = "thiserror", derive(thiserror::Error))]
+pub enum ParseAssetError {
+    /// Contains the asset delimiter.
+    #[cfg_attr(feature = "thiserror", error("contains `-`"))]
+    ContainsSep,
+    /// Empty str.
+    #[cfg_attr(feature = "thiserror", error("empty str cannot be asset"))]
+    Empty,
+    /// Contains non-ascii characters.
+    #[cfg_attr(feature = "thiserror", error("contains non-ascii characters"))]
+    NonAscii,
+}
 
-impl From<Str> for Asset {
-    fn from(value: Str) -> Self {
-        let s = value.to_uppercase();
-        if s == value {
-            Self { inner: value }
+#[cfg(not(feature = "thiserror"))]
+impl fmt::Display for ParseAssetError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::ContainsSep => write!(f, "contains `-`"),
+            Self::Empty => write!(f, "empty str cannot be asset"),
+            Self::NonAscii => write!(f, "contains non-ascii characters"),
+        }
+    }
+}
+
+impl<'a> TryFrom<&'a str> for Asset {
+    type Error = ParseAssetError;
+
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
+        if value.is_empty() {
+            Err(ParseAssetError::Empty)
+        } else if value.contains(Self::SEP) {
+            Err(ParseAssetError::ContainsSep)
+        } else if !value.is_ascii() {
+            Err(ParseAssetError::NonAscii)
         } else {
-            Self {
-                inner: Str::from(s),
-            }
+            Ok(Self {
+                inner: Str::new(value.to_ascii_uppercase()),
+            })
         }
     }
 }
 
-impl From<String> for Asset {
-    #[inline]
-    fn from(s: String) -> Self {
-        Self {
-            inner: Str::from(s.to_uppercase()),
-        }
-    }
-}
+impl TryFrom<Str> for Asset {
+    type Error = ParseAssetError;
 
-impl<'a> From<&'a str> for Asset {
-    #[inline]
-    fn from(s: &'a str) -> Self {
-        Self {
-            inner: Str::from(s.to_uppercase()),
-        }
-    }
-}
-
-impl<'a> From<&'a Str> for Asset {
-    fn from(value: &'a Str) -> Self {
-        let s = value.to_uppercase();
-        if s == *value {
-            Self {
-                inner: value.clone(),
-            }
-        } else {
-            Self {
-                inner: Str::from(s),
-            }
-        }
+    fn try_from(value: Str) -> Result<Self, Self::Error> {
+        Self::try_from(value.as_str())
     }
 }
 
 impl FromStr for Asset {
-    type Err = core::convert::Infallible;
+    type Err = ParseAssetError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Self::from(s))
+        Self::try_from(s)
     }
 }
 
@@ -109,6 +108,8 @@ impl<'a> Borrow<str> for &'a Asset {
 }
 
 impl Asset {
+    /// The delimiter of assets in the spot format.
+    pub const SEP: char = '-';
     /// Usdt.
     pub const USDT: Self = Self::new_inline("USDT");
     /// Usd.
@@ -189,13 +190,13 @@ mod tests {
 
     #[test]
     fn from_str() {
-        let asset = Asset::from("usdt");
+        let asset = Asset::from_str("usdt").unwrap();
         assert_eq!(asset.as_str(), "USDT");
     }
 
     #[test]
     fn equal() {
-        let asset = Asset::from("usdt");
+        let asset = Asset::from_str("usdt").unwrap();
         assert_eq!(asset, Asset::usdt());
         assert_eq!(asset, *"usdt");
         assert_eq!(asset, "usdt");
--- src/instrument.rs
+++ src/instrument.rs
@@ -1,9 +1,17 @@
-use core::{borrow::Borrow, hash::Hash};
+use core::{borrow::Borrow, hash::Hash, str::FromStr};
 
+use crate::{
+    asset::{Asset, ParseAssetError},
+    prelude::Str,
+    IntoNaivePosition, Position, PositionNum,
+};
 use alloc::fmt;
-use smol_str::SmolStr as Str;
 
-use crate::{asset::Asset, IntoNaivePosition, Position, PositionNum};
+#[cfg(not(feature = "std"))]
+use alloc::{
+    format,
+    string::{String, ToString},
+};
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
@@ -17,6 +25,159 @@ pub struct Instrument {
     pub(crate) symbol: Str,
     base: Asset,
     quote: Asset,
+}
+
+/// Symbol.
+#[derive(Debug, Clone, PartialOrd, Ord, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde", serde(try_from = "Str", into = "String"))]
+pub struct Symbol {
+    prefix: Str,
+    symbol: Str,
+}
+
+impl Symbol {
+    /// Empty str, the prefix of spot instruments.
+    pub const SPOT_PREFIX: Str = Str::new_inline("");
+
+    /// The delimiter of prefix and symbol.
+    pub const SEP: char = ':';
+
+    /// Get prefix.
+    /// The prefix of spot instrument must be empty.
+    pub fn prefix(&self) -> &str {
+        self.prefix.as_str()
+    }
+
+    /// Is spot.
+    pub fn is_spot(&self) -> bool {
+        self.prefix.is_empty()
+    }
+
+    /// Get symbol.
+    /// The symbol of spot instrument must be of this format: `"{base}-{quote}"`,
+    /// where `base` and `quote` are in upppercase. For example, `"BTC-USDT"` is a
+    /// valid spot symbol.
+    pub fn symbol(&self) -> &str {
+        self.symbol.as_str()
+    }
+
+    /// Create a symbol for the spot.
+    pub fn spot(base: &Asset, quote: &Asset) -> Self {
+        Self {
+            prefix: Self::SPOT_PREFIX,
+            symbol: Str::new(format!("{base}{}{quote}", Asset::SEP)),
+        }
+    }
+
+    /// Create a new symbol from [`Str`].
+    /// Return `None` if `prefix` is emtpy or contains [`Symbol::SEP`];
+    pub fn from_smol_str(prefix: &Str, symbol: &Str) -> Option<Self> {
+        if prefix.is_empty() || prefix.contains(Self::SEP) {
+            return None;
+        }
+        Some(Self {
+            prefix: prefix.clone(),
+            symbol: symbol.clone(),
+        })
+    }
+
+    /// Create a new symbol from text.
+    /// Return `None` if `prefix` is emtpy or contains [`Symbol::SEP`];
+    pub fn new(prefix: &str, symbol: &str) -> Option<Self> {
+        if prefix.is_empty() || prefix.contains(Self::SEP) {
+            return None;
+        }
+        Some(Self {
+            prefix: Str::new(prefix),
+            symbol: Str::new(symbol),
+        })
+    }
+}
+
+impl From<Symbol> for String {
+    fn from(symbol: Symbol) -> Self {
+        symbol.to_string()
+    }
+}
+
+impl fmt::Display for Symbol {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.prefix.is_empty() {
+            write!(f, "{}", self.symbol)
+        } else {
+            write!(f, "{}{}{}", self.prefix, Self::SEP, self.symbol)
+        }
+    }
+}
+
+/// Parse symbol error.
+#[derive(Debug)]
+#[cfg_attr(feature = "thiserror", derive(thiserror::Error))]
+pub enum ParseSymbolError {
+    /// Invalid spot format.
+    #[cfg_attr(feature = "thiserror", error("invalid spot format"))]
+    InvalidSpotFormat,
+    /// Invalid prefix.
+    #[cfg_attr(feature = "thiserror", error("invalid prefix"))]
+    InvalidPrefix,
+    /// Asset errors.
+    #[cfg_attr(feature = "thiserror", error("parse asset error: {0}"))]
+    Asset(ParseAssetError),
+}
+
+#[cfg(not(feature = "thiserror"))]
+impl fmt::Display for ParseSymbolError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::InvalidSpotFormat => write!(f, "invalid spot format"),
+            Self::InvalidPrefix => write!(f, "invalid prefix"),
+            Self::Asset(err) => write!(f, "parse asset error: {err}"),
+        }
+    }
+}
+
+impl From<ParseAssetError> for ParseSymbolError {
+    fn from(err: ParseAssetError) -> Self {
+        Self::Asset(err)
+    }
+}
+
+impl<'a> TryFrom<&'a str> for Symbol {
+    type Error = ParseSymbolError;
+
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
+        match value.split_once(Self::SEP) {
+            Some((prefix, symbol)) => {
+                Self::new(prefix, symbol).ok_or(ParseSymbolError::InvalidPrefix)
+            }
+            None => {
+                if let Some((base, quote)) = value.split_once(Asset::SEP) {
+                    let base = Asset::from_str(base)?;
+                    let quote = Asset::from_str(quote)?;
+                    Ok(Self::spot(&base, &quote))
+                } else {
+                    Err(ParseSymbolError::InvalidSpotFormat)
+                }
+            }
+        }
+    }
+}
+
+impl TryFrom<Str> for Symbol {
+    type Error = ParseSymbolError;
+
+    fn try_from(value: Str) -> Result<Self, Self::Error> {
+        Self::try_from(value.as_str())
+    }
+}
+
+impl FromStr for Symbol {
+    type Err = ParseSymbolError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::try_from(s)
+    }
 }
 
 impl Instrument {
@@ -135,5 +296,41 @@ impl Borrow<str> for Instrument {
 impl<'a> Borrow<str> for &'a Instrument {
     fn borrow(&self) -> &str {
         self.symbol()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn symbol_from_str() {
+        let swap = Symbol::from_str("SWAP:btcusdt").unwrap();
+        assert!(!swap.is_spot());
+        assert_eq!(swap, Symbol::new("SWAP", "btcusdt").unwrap());
+        assert_eq!(swap.to_string(), "SWAP:btcusdt");
+        let spot = Symbol::from_str("btc-usdt").unwrap();
+        assert!(spot.is_spot());
+        assert_eq!(spot, Symbol::spot(&Asset::BTC, &Asset::USDT));
+        assert_eq!(spot.to_string(), "BTC-USDT");
+    }
+
+    #[cfg(feature = "serde")]
+    #[test]
+    fn symbol_serde() -> anyhow::Result<()> {
+        use alloc::{vec, vec::Vec};
+
+        let value = serde_json::json!(["futures:BTC-USDT-210101", "USDT-BTC"]);
+        let assets: Vec<Symbol> = serde_json::from_value(value)?;
+        assert_eq!(
+            assets,
+            [
+                Symbol::new("futures", "BTC-USDT-210101").unwrap(),
+                Symbol::spot(&Asset::USDT, &Asset::BTC)
+            ]
+        );
+        let s = serde_json::to_string(&assets)?;
+        assert_eq!(s, r#"["futures:BTC-USDT-210101","USDT-BTC"]"#);
+        Ok(())
     }
 }
